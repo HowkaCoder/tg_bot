f1496ac197985d0ab55642dec08b9f340f1eaaa9
Howka <prostojproger@gmail.com>
2024-12-11 21:54:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
+from aiogram.enums import ParseMode
 from aiogram.utils import executor
 import sqlite3
 import logging
